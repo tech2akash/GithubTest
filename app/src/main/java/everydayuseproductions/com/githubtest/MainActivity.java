952c806f59e055bd15c1ad0862ed4cdfcdde9157
Akashdeep Singh <deep2sunny@gmail.com>
2016-09-04 15:15:36 +0530

    text_added_branch

--- app/src/main/java/everydayuseproductions/com/githubtest/MainActivity.java
+++ app/src/main/java/everydayuseproductions/com/githubtest/MainActivity.java
@@ -2,15 +2,19 @@ package everydayuseproductions.com.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt = (TextView) findViewById(R.id.mytext);
+        txt.setText("Hello GitHub");
     }
 }
