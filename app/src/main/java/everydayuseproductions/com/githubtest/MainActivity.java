5c9956d404ad6a6da5d0c6d0048a40e7e387b47c
Akashdeep Singh <deep2sunny@gmail.com>
2016-09-04 15:36:37 +0530

    Merge remote-tracking branch 'remotes/origin/Adding_text'
    # Conflicts:
    #	app/src/main/java/everydayuseproductions/com/githubtest/MainActivity.java

--- app/src/main/java/everydayuseproductions/com/githubtest/MainActivity.java
+++ app/src/main/java/everydayuseproductions/com/githubtest/MainActivity.java
@@ -2,6 +2,7 @@ package everydayuseproductions.com.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,6 @@ public class MainActivity extends AppCompatActivity {
         txt.setText("Hello GitHub");
 
         Log.i("","");
-        Log.d("","");
+      //  Log.d("","");
     }
 }
