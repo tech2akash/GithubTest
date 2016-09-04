6754c218f815e784a5004c25cbb521aead688dd7
Akashdeep Singh <deep2sunny@gmail.com>
2016-09-04 15:26:49 +0530

    log added to branch

--- app/src/main/java/everydayuseproductions/com/githubtest/MainActivity.java
+++ app/src/main/java/everydayuseproductions/com/githubtest/MainActivity.java
@@ -2,6 +2,7 @@ package everydayuseproductions.com.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         txt = (TextView) findViewById(R.id.mytext);
         txt.setText("Hello GitHub");
+
+        Log.i("","");
     }
 }
